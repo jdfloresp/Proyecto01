daaa7262fe713648cfdf58f35790e06fd88060a3
jdfloresp <diego_floresp@hotmail.com>
2024-10-04 17:15:27 -0500

    Update bdbanco.sql

--- bdbanco.sql
+++ bdbanco.sql
@@ -1,5 +1,14 @@
 use bdbanco;
 
+CREATE TABLE cliente (
+  Clienteid int NOT NULL AUTO_INCREMENT,
+  nombres varchar(255) NOT NULL,
+  apellidos varchar(255) DEFAULT NULL,
+  dni varchar(255) DEFAULT NULL,
+  email varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Clienteid`)
+);
+
 select * from cliente;
 
 select * from cuentabancaria;
@@ -12,19 +21,7 @@ saldo varchar(30),
 tipocuenta varchar(10)
 );
 
-drop table cuentabancaria
-create TABLE cuentabancaria(
-idCuenta int primary key AUTO_INCREMENT,
-datoscliente varchar(100),
-numcuenta varchar(30) not null,
-saldo varchar(30),
-estado float,
-tipocuenta varchar(10)
-);
-
 insert into cuentabancaria (datoscliente,numcuenta,saldo,tipocuenta) 
-values ("Giron Paulo","12345678","2000","Soles")
-
-select * from cliente
+values ("Pablo David Perez","12345678","2000","Soles")
 
 
